91012107cd63d509fb1f184fc719c8b4c8899691
Jeffv <jefersonalonsov@gmail.com>
2022-02-25 18:54:46 -0500

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConceptsPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("POO Concepts");
+            Console.WriteLine("-----------");
+
+            try
+            {
+                Console.WriteLine(new Date(2022, 2, 3));
+                Console.WriteLine(new Date(2022, 9, 3));
+                Console.WriteLine(new Date(2022, 3, 34));
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+    }
+}
