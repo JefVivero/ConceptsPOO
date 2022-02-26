01ddc34b7213bb4bb651c64de8d3903dede16ad8
Jeffv <jefersonalonsov@gmail.com>
2022-02-25 20:27:47 -0500

    Finish Commission and Salary Employee

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -9,7 +9,33 @@ namespace ConceptsPOO
             Console.WriteLine("POO Concepts");
             Console.WriteLine("-----------");
 
-            try
+            Employee Empleado1 = new SalaryEmployee()
+            {
+                Id = 1,
+                FirstName = "Pepito",
+                LastName = "Perez",
+                BirthDate = new Date(1986, 12, 18),
+                HiringDate = new Date(2021, 6, 1),
+                IsActive = true,
+                Salary = 1200000.50M
+            };
+
+            Employee Empleado2 = new CommissionEmployee()
+            {
+                Id = 1,
+                FirstName = "Juana",
+                LastName = "Rodallega",
+                BirthDate = new Date(1999, 7, 4),
+                HiringDate = new Date(2022, 1, 15),
+                IsActive = true,
+                Sales = 430000000M,                
+                CommissionPercentaje = 0.03F
+            };
+
+            Console.WriteLine(Empleado1);
+            Console.WriteLine(Empleado2);
+
+            /*try       -----Proof Date Class
             {
                 Console.WriteLine(new Date(2022, 2, 3));
                 Console.WriteLine(new Date(2022, 9, 3));
@@ -18,7 +44,7 @@ namespace ConceptsPOO
             catch (Exception error)
             {
                 Console.WriteLine(error.Message);
-            }
+            }*/
         }
     }
 }
